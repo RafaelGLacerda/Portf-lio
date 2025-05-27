5e768e570d7c7023528427b9f7908c954dd8c9a6
Rafael Lacerda <Rafaellacerda2004@gmail.com>
2025-05-27 20:38:45 -0300

    navbar removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import About from './components/About';
 import Skills from './components/Skills';
 
+
 const App: React.FC = () => {
   return (
     <div className="bg-gray-900 text-white font-sans">
-      <Navbar />
       <Header />
       <About />
       <Projects />
